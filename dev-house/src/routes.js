2922c1d2a44a30ca1525e430cff335889c433768
Diêgo Ferreira <diegofrr.19@gmail.com>
2022-10-16 04:16:55 -0300

    dev-house project - initial commit

---
+++ dev-house/src/routes.js
@@ -0,0 +1,11 @@
+// const { Router } = require('express');
+
+import { Router } from 'express';
+
+const routes = new Router();
+
+routes.get('/', (req, res) => {
+    return res.json({ok: true})
+});
+
+export default routes;
