fba111c40df999db5c554395480b3e3534d9b2cd
Diêgo Ferreira <diegofrr.19@gmail.com>
2022-10-24 07:20:35 -0300

    update

--- new-project/src/routes.js
+++ new-project/src/routes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 const routes = new Router();
 
-routes.get("/test", (request, response) => response.json({ ok: true }));
+routes.get("/test", (request, response) => {
+    response.json({ ok: true });
+});
 
 export default routes;
