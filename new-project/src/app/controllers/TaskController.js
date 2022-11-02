bbd494040d79e5acecad5611083184a128e9aafb
Diêgo Ferreira <diegofrr.19@gmail.com>
2022-11-02 10:26:00 -0300

    update and delete task

--- new-project/src/app/controllers/TaskController.js
+++ new-project/src/app/controllers/TaskController.js
@@ -30,4 +30,32 @@ export default new (class TaskController {
                 response.status(400).json(formattedSchemaError(err))
             );
     }
+
+    async update(request, response) {
+        const { task_id } = request.params;
+        const task = await Task.findByPk(task_id);
+        if (!task) {
+            return response
+                .status(400)
+                .json({ eror: 'Tarefa não encontrada.' });
+        }
+        if (task.user_id !== request.userId) {
+            return response.status(401).json({ error: 'Não autorizado' });
+        }
+        await task.update(request.body);
+        return response.json(task);
+    }
+
+    async delete(request, response) {
+        const task = await Task.findByPk(request.params.task_id);
+        if (!task) {
+            return response.json({ error: 'Tarefa não existe.' });
+        }
+        if (task.user_id !== request.userId) {
+            return response.status(401).json({ error: 'Não autorizado.' });
+        }
+
+        await task.destroy();
+        return response.send();
+    }
 })();
