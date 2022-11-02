a8029ce9300c143cde75504de0333fd06b98448f
Diêgo Ferreira <diegofrr.19@gmail.com>
2022-11-02 13:49:58 -0300

    errors/AppError.ts

--- course_02/sales-api/src/shared/http/server.ts
+++ course_02/sales-api/src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from './errors/AppError';
 
 const server = express();
 
@@ -8,6 +9,27 @@ server.use(cors());
 server.use(express.json());
 
 server.use(routes);
+
+server.use(
+    (
+        error: Error,
+        request: Request,
+        response: Response,
+        next: NextFunction,
+    ) => {
+        if (error instanceof AppError) {
+            return response.status(error.statusCode).json({
+                status: 'error',
+                message: error.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal server error',
+        });
+    },
+);
 
 server.listen(3333, () => {
     console.log('🟢 Server started on port 3333!');
